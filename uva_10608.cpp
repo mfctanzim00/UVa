a7cb7d0e126299f45a54cc2d4961bc9ae30a8f16
mfctanzim00 <69580317+mfctanzim00@users.noreply.github.com>
2020-11-05 14:07:58 +0600

    Create uva_10608.cpp

---
+++ uva_10608.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#include<cstring>
+//#define Mfc_Tanzim
+#define   ll         long long
+#define   ull        unsigned long long
+#define   pb         push_back
+#define   ff         first
+#define   ss         second
+#define   all(v)     (v.begin(), v.end())
+#define   rall(v)    (v.rbegin(), v.rend())
+#define   pi         acos(-1.0)
+#define   FastRead   ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define   bug(a)     cerr << #a << " : " << a << endl
+using namespace std;
+const ll mod = 1e9+7;
+const ll mx = 2e6+10;
+
+int posx[] = {1,-1, 0, 0};
+int posy[] = {0, 0, 1,-1};
+ll parent[mx], child_cnt[mx];
+
+ll Find_par(ll r)
+{
+    if(parent[r]==r)
+        return r;
+    return parent[r] = Find_par(parent[r]);
+}
+
+int main()
+{
+    FastRead
+
+#ifdef Mfc_Tanzim
+    freopen("input.txt","r", stdin);
+    // freopen("output.txt","w", stdout);
+#endif /// Mfc_Tanzim
+
+    ll t=1, n, k, m, a, b, cnt=0, d;
+    cin >> t;
+
+    while(t--){
+            cin >> n >> m;
+            for(ll i=1; i<=n; i++){
+                    parent[i] = i;
+                    child_cnt[i] = 1;
+            }
+            while(m--){
+                    ll u, v;
+                    cin >> u >> v;
+
+                    ll uPr = Find_par(u), vPr = Find_par(v);
+                    if(uPr!=vPr){
+                              parent[uPr] = vPr;
+                              child_cnt[vPr] += child_cnt[uPr];
+                    }
+            }
+            ll mx = 0;
+            for(ll i=1; i<=n; i++){
+                    mx = max(mx, child_cnt[i]);
+            }
+            cout << mx << '\n';
+    }
+    return 0;
+}
+
